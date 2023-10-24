45333fd6e2916cdd3dec477b3e66180d8482ffc3
João Pedro <joaopedrowinter10@gmail.com>
2023-10-24 17:19:32 -0300

    Arrumando Consulta na Model Time.php

--- app/Models/Time.php
+++ app/Models/Time.php
@@ -24,26 +24,26 @@ class Time extends Model
             ELSE vc_partidas.time2_gols1 + vc_partidas.time2_gols2
         END) AS gols
     '), DB::raw('
-        COUNT(DISTINCT vc_partidas.id) AS jogos
+        COUNT(DISTINCT vc_partidas.id)*2 AS jogos 
     '), DB::raw('
         SUM(CASE 
-            WHEN (vc_partidas.time1_id = vc_times.id AND (vc_partidas.time1_gols1 > vc_partidas.time2_gols1 OR vc_partidas.time1_gols2 > vc_partidas.time2_gols2)) OR
-                 (vc_partidas.time2_id = vc_times.id AND (vc_partidas.time2_gols1 > vc_partidas.time1_gols1 OR vc_partidas.time2_gols2 > vc_partidas.time1_gols2))
+            WHEN (vc_partidas.time1_id = vc_times.id AND (vc_partidas.time1_gols1 > vc_partidas.time2_gols1 OR vc_partidas.time1_gols2 > vc_partidas.time2_gols2 AND vc_partidas.time1_gols1 < vc_partidas.time2_gols1 OR vc_partidas.time1_gols2 < vc_partidas.time2_gols2)) OR
+                 (vc_partidas.time2_id = vc_times.id AND (vc_partidas.time2_gols1 > vc_partidas.time1_gols1 OR vc_partidas.time2_gols2 > vc_partidas.time1_gols2 AND vc_partidas.time2_gols1 < vc_partidas.time1_gols1 OR vc_partidas.time2_gols2 < vc_partidas.time1_gols2))
                   THEN 1
             ELSE 0 
         END) AS vitorias
     '), DB::raw('
         SUM(CASE 
-            WHEN (vc_partidas.time1_id = vc_times.id AND (vc_partidas.time1_gols1 < vc_partidas.time2_gols1 OR vc_partidas.time1_gols2 < vc_partidas.time2_gols2)) OR
-                 (vc_partidas.time2_id = vc_times.id AND (vc_partidas.time2_gols1 < vc_partidas.time1_gols1 OR vc_partidas.time2_gols2 < vc_partidas.time1_gols2)) THEN 1
+            WHEN (vc_partidas.time1_id = vc_times.id AND (vc_partidas.time1_gols1 < vc_partidas.time2_gols1 OR vc_partidas.time1_gols2 < vc_partidas.time2_gols2 AND vc_partidas.time1_gols1 > vc_partidas.time2_gols1 OR vc_partidas.time1_gols2 > vc_partidas.time2_gols2)) OR
+                 (vc_partidas.time2_id = vc_times.id AND (vc_partidas.time2_gols1 < vc_partidas.time1_gols1 OR vc_partidas.time2_gols2 < vc_partidas.time1_gols2 AND vc_partidas.time2_gols1 > vc_partidas.time1_gols1 OR vc_partidas.time2_gols2 > vc_partidas.time1_gols2)) THEN 1
             ELSE 0 
         END) AS derrotas
     '), DB::raw('
         SUM(CASE 
-            WHEN (vc_partidas.time1_id = vc_times.id AND ((vc_partidas.time1_gols1 = vc_partidas.time2_gols1 AND vc_partidas.time1_gols2 = vc_partidas.time2_gols2) OR (vc_partidas.time1_gols1 = vc_partidas.time2_gols2 AND vc_partidas.time1_gols2 = vc_partidas.time2_gols1))) OR
-                 (vc_partidas.time2_id = vc_times.id AND ((vc_partidas.time2_gols1 = vc_partidas.time1_gols1 AND vc_partidas.time2_gols2 = vc_partidas.time1_gols2) OR (vc_partidas.time2_gols1 = vc_partidas.time1_gols2 AND vc_partidas.time2_gols2 = vc_partidas.time1_gols1))) THEN 1
+            WHEN (vc_partidas.time1_id = vc_times.id AND ((vc_partidas.time1_gols1 = vc_partidas.time2_gols1 OR vc_partidas.time1_gols2 = vc_partidas.time2_gols2) OR (vc_partidas.time1_gols1 = vc_partidas.time2_gols1 OR vc_partidas.time1_gols2 = vc_partidas.time2_gols2))) OR
+                 (vc_partidas.time2_id = vc_times.id AND ((vc_partidas.time2_gols1 = vc_partidas.time1_gols1 OR vc_partidas.time2_gols2 = vc_partidas.time1_gols2) OR (vc_partidas.time2_gols1 = vc_partidas.time1_gols1 OR vc_partidas.time2_gols2 = vc_partidas.time1_gols2))) THEN 1
             ELSE 0 
-        END) AS empates
+        END)*2 AS empates
     '))
             ->leftJoin('vc_partidas', function ($join) {
                 $join->on('vc_times.id', '=', 'vc_partidas.time1_id')
